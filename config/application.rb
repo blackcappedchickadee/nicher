e8bf2fa585d93f9c4e318f692fbd46b38e223d8a
Chirpmaster Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-02-26 13:40:21 -0500

    Added User model and views

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,10 @@ module Nicher
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # For Heroku
+    # config.assets.initialize_on_precompile = false
+    
+    
   end
 end
