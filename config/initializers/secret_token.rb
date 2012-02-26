fb56f8ed499aa425777cc9d515ee032b6f8d5fcf
Chirpmaster Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-02-26 12:29:02 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nicher::Application.config.secret_token = '52a2c47b7b1bd4fe4549ca87084c2a56d5d9488db930fcac09790ff038e37a382f50e977e056c6cdf10d58e6105183b3d984a9fd617abedc07a84dfc322d2014'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nicher::Application.config.session_store :cookie_store, key: '_nicher_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nicher::Application.config.session_store :active_record_store
