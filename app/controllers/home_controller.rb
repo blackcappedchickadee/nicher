ec93a74498e1bb62dbdce6c963f6fd413522cf5e
Chirpmaster Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-02-26 14:53:22 -0500

    Added list of files to homepage, added upload button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController  
     
   def index  
-     
+     if user_signed_in?  
+       @assets = current_user.assets.order("uploaded_file_file_name desc")        
+      end
   end  
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Nicher::Application.initialize!
+
+#Formatting DateTime to look like "01/20/2012 10:28PM"  
+Time::DATE_FORMATS[:default] = "%m/%d/%Y %l:%M%p"
